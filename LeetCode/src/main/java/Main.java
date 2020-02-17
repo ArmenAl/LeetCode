c4301b4502761b31bd96e6e5299ece526c230dfa
ArmenAl <armen.al@gmail.com>
2020-02-17 13:11:07 +0100

    LeetCode1342

--- LeetCode/src/main/java/Main.java
+++ LeetCode/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
-
+// Leet Code 1342
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Skriv in tal: ");
