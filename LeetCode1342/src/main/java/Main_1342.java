cece61c2b0eebc9dad6f8a55c13533dfcf542a70
ArmenAl <armen.al@gmail.com>
2020-02-17 13:38:09 +0100

    LeetCode1342 LeetCode1108

--- LeetCode1342/src/main/java/Main_1342.java
+++ LeetCode1342/src/main/java/Main_1342.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main_1342 {
 // Leet Code 1342
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
